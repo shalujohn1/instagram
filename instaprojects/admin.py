4a7f22a87ac8aec187ca96b231187b031e8ba8fe
shalu john <shalu@yougotagift.com>
2023-10-11 11:18:56 +0530

    instaprojects/admin.py

--- instaprojects/admin.py
+++ instaprojects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post,Like,Comment
+
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+
+
 
 # Register your models here.
