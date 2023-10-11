d86c7e954e2ee8813c97ff27266258a0dc39c500
shalu john <shalu@yougotagift.com>
2023-10-11 11:35:40 +0530

    instaprojects/utils.py not used

--- instaprojects/utils.py
+++ instaprojects/utils.py
@@ -1,20 +1,21 @@
-from users.models import Profile
-from instaprojects.models import Post
-from django.db.models import Q
-
-def searchProfiles(request):
-    search_query = ''
-
-    if request.GET.get('search_query'):
-        search_query = request.GET.get('search_query')
-
-    post = Post.objects.distinct().filter(
-        Q(description__icontains=search_query)
-    )
-
-    profile = Profile.objects.distinct().filter(
-        Q(name__icontains=search_query)
-    )
-    print(search_query)
-    print(profile)
-    return profile, post, search_query
+# from users.models import Profile
+# from instaprojects.models import Post
+# from django.db.models import Q
+# from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+#
+# def searchProfiles(request):
+#     search_query = ''
+#
+#     if request.GET.get('search_query'):
+#         search_query = request.GET.get('search_query')
+#
+#     post = Post.objects.distinct().filter(
+#         Q(description__icontains=search_query)
+#     )
+#
+#     profile = Profile.objects.distinct().filter(
+#         Q(name__icontains=search_query)
+#     )
+#     print(search_query)
+#     print(profile)
+#     return profile, post, search_query
