51e54da111b85f98cec4587fdb841bf6c4db98d5
shalu john <shalu@yougotagift.com>
2023-10-16 17:38:10 +0530

    instagram/settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -40,8 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'instaprojects',
     'users',
+    'rest_framework',
 
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
