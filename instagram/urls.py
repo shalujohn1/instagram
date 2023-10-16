d8159b38d882bf6ea1aef431f39e8f7574315df2
shalu john <shalu@yougotagift.com>
2023-10-16 17:38:25 +0530

    instagram/urls.py

--- instagram/urls.py
+++ instagram/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('instaprojects.urls')),
     path('users/', include('users.urls')),
+    path('api/', include('api.urls'))
 ]
 
 
